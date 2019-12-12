99c4f8ab161421b844de163055b8dea4c479bcfa
Pawel Matusz <pawelmatusz@MacBook-Air-Pawel.local>
2019-12-12 16:50:45 +0100

    add

---
+++ dist/js/slider.js
@@ -0,0 +1,28 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1500,
+    effect: 'slide',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    autoplay: {
+        delay: 6000,
+        waitForTransition: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
