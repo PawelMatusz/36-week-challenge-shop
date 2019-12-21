916637d1d4f7ee4c65ff4ae7ef172dab97e3985d
Pawel Matusz <pawelmatusz@MacBook-Air-Pawel.local>
2019-12-21 23:26:30 +0100

    async add products

--- dist/js/shop.js
+++ dist/js/shop.js
@@ -1,10 +1,30 @@
-const productsIndex = document.querySelector('.products__index');
+const getProducts = function() {
+  let productsList = new XMLHttpRequest();
+  productsList.onload = function() {
+    let visibleProduct = 5;
+    if (productsList.status === 200) {
+      const products = JSON.parse(this.responseText);
 
-function products() {
-  req = new XMLHttpRequest();
-  req.open('GET', 'products.json', true);
-  req.send(null);
-  req.responseType = 'json';
-}
+      for (let i = 0; i < products.products.length; i++) {
+        const productsIndex = document.querySelector('.products__index');
+        const productCard = document.createElement('div');
+        productCard.setAttribute('class', 'product__card');
+        productCard.innerHTML = `<img class="product__card-photo" src=${products.products[i].image}></img>
+      <p class="product__card-description">${products.products[i].description}</p>
+      <p class="product__card-price">${products.products[i].price}</p>
+      <button class="product__card-add">
+      <span><img class="product__card-add-cart" src="/dist/img/cart.png" alt=""/></span>
+      Add to Cart </button>`;
+        productsIndex.appendChild(productCard);
+        if (i === visibleProduct) return;
+      }
+    } else if (productsList.status === 404) {
+      throw 'err';
+    }
+  };
 
-console.log(products);
+  productsList.open('GET', `dist/json/product.json`, true);
+  productsList.send(null);
+};
+
+getProducts();
