8d411f2fd83e36090900996700accd7c4054aa5e
Pawel Matusz <pawelmatusz@MacBook-Air-Pawel.local>
2019-12-22 22:03:33 +0100

    change shop load

--- dist/js/shop.js
+++ dist/js/shop.js
@@ -8,14 +8,23 @@ const getProducts = function() {
       for (let i = 0; i < products.products.length; i++) {
         const productsIndex = document.querySelector('.products__index');
         const productCard = document.createElement('div');
+        const productImage = document.createElement('img');
+        const productDescription = document.createElement('p');
+        const productPrice = document.createElement('p');
+        const addCart = document.createElement('button');
         productCard.setAttribute('class', 'product__card');
-        productCard.innerHTML = `<img class="product__card-photo" src=${products.products[i].image}></img>
-      <p class="product__card-description">${products.products[i].description}</p>
-      <p class="product__card-price">${products.products[i].price}</p>
-      <button class="product__card-add">
-      <span><img class="product__card-add-cart" src="/dist/img/cart.png" alt=""/></span>
-      Add to Cart </button>`;
+        productImage.setAttribute('class', 'product__card-photo');
+        productImage.setAttribute('src', `${products.products[i].image}`);
+        productDescription.setAttribute('class', 'product__card-description');
+        productDescription.textContent = `${products.products[i].description}`;
+        productPrice.setAttribute('class', 'product__card-price');
+        productPrice.textContent = `${products.products[i].price}`;
+        productCard.appendChild(productImage);
+        productCard.appendChild(productDescription);
+        productCard.appendChild(productPrice);
+
         productsIndex.appendChild(productCard);
+
         if (i === visibleProduct) return;
       }
     } else if (productsList.status === 404) {
