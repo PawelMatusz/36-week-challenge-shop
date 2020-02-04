4c64ecef308a3df210f1b341a81e2960f6e10f98
Paweu <pawel.matusz94@gmail.com>
2020-02-04 02:55:49 +0100

    in progress

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector('.nav__icon');
 const header = document.querySelector('.header');
+const menuItem = document.querySelectorAll('.header__nav-link');
 
 menuIcon.addEventListener('click', function() {
   header.classList.toggle('is-nav-open');
@@ -7,6 +8,14 @@ menuIcon.addEventListener('click', function() {
 
 menuItem.forEach(function(item) {
   item.addEventListener('click', () => {
-    menuIcon.classList.remove('open');
+    menuIcon.classList.remove('is-nav-open');
   });
+});
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 70) {
+    header.classList.add('is-scroll');
+  } else {
+    header.classList.remove('is-scroll');
+  }
 });
