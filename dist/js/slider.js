8de71cebe1d169bc71d47b268b3ad192355851ec
Paweu <pawel.matusz94@gmail.com>
2020-04-06 12:50:13 +0200

    add new font fix some functionality

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,28 +1,29 @@
 var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 1500,
-    effect: 'slide',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 1500,
+  effect: 'slide',
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
 
-    autoplay: {
-        delay: 6000,
-        waitForTransition: true,
-    },
+  autoplay: {
+    delay: 6000,
+    waitForTransition: true,
+  },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-})
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
