bdc453acdae1e75965ed89a912d3a9c5c25d03ac
Paweu <pawel.matusz94@gmail.com>
2020-05-21 17:24:31 +0200

    simple newsletter validation

--- dist/js/handleSite.js
+++ dist/js/handleSite.js
@@ -4,13 +4,18 @@ const Selectors = {
   item: '[data-nav-link]',
   shopButton: '[data-shop-button]',
   shopMenu: '[data-shop-menu]',
+  newsletterForm: '[data-newsletter-form]',
+  newsletterInputEmail: '[data-newsletter-email]',
+  newsletterbutton: '[data-newsletter-button]',
 };
 
 const navIcon = document.querySelector(Selectors.icon),
   header = document.querySelector(Selectors.header),
   navItem = document.querySelectorAll(Selectors.item),
   shopButton = document.querySelector(Selectors.shopButton),
-  shopMenu = document.querySelector(Selectors.shopMenu);
+  shopMenu = document.querySelector(Selectors.shopMenu),
+  newsletterForm = document.querySelector(Selectors.newsletterForm),
+  newsletterInputEmail = document.querySelector(Selectors.newsletterInputEmail);
 
 navIcon.addEventListener('click', () => {
   header.classList.toggle('is-nav-open');
@@ -32,4 +37,14 @@ window.addEventListener('scroll', () => {
 
 shopButton.addEventListener('click', () => {
   shopMenu.classList.toggle('is-menu-open');
+});
+
+newsletterForm.addEventListener('submit', e => {
+  console.log(newsletterForm);
+  e.preventDefault();
+  if (newsletterInputEmail.value.length >= 3) {
+    alert(`${newsletterInputEmail.value} zapisany do newslettera!!!`);
+  } else {
+    alert('Kolego wypełniłeś błędnie nasz super formularz');
+  }
 });
